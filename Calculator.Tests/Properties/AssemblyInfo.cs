43f3f9c38bc0d0b92728cdc6ec1df4c501f34efe
jzacka1 <jameszacka@gmail.com>
2019-02-01 22:21:57 -0500

    Added Test Methods to Evaluate Methods in Calculator Service.

---
+++ Calculator.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Calculator.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Calculator.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35057272-2479-47f0-b369-b66987ea730b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Calculator.Tests/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DevOps_Test_Demo_Project.Calculator;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAdding()
+        {
+            //Arrange
+            DevOps_Test_Demo_Project.Calculator.Calculator calculator = new DevOps_Test_Demo_Project.Calculator.Calculator();
+
+            // Act
+            int integerResult = calculator.Add(4, 2);
+
+            //Assert
+            Assert.IsNotNull(integerResult); // Testing integers
+            Assert.AreEqual(6, integerResult); 
+        }
+
+        [TestMethod]
+        public void TestSubtracting()
+        {
+            //Arrange
+            DevOps_Test_Demo_Project.Calculator.Calculator calculator = new DevOps_Test_Demo_Project.Calculator.Calculator();
+
+            // Act
+            int integerResult = calculator.Subtract(4, 2);
+
+            //Assert
+            Assert.IsNotNull(integerResult);
+            Assert.AreEqual(2, integerResult); // Testing integers
+        }
+
+        [TestMethod]
+        public void TestMultiplying()
+        {
+            //Arrange
+            DevOps_Test_Demo_Project.Calculator.Calculator calculator = new DevOps_Test_Demo_Project.Calculator.Calculator();
+
+            // Act
+            int integerResult = calculator.Multiply(4, 2);
+
+            //Assert
+            Assert.IsNotNull(integerResult);
+            Assert.AreEqual(8, integerResult); // Testing integers
+        }
+
+        [TestMethod]
+        public void TestDividing()
+        {
+            //Arrange
+            DevOps_Test_Demo_Project.Calculator.Calculator calculator = new DevOps_Test_Demo_Project.Calculator.Calculator();
+
+            // Act
+            int integerResult = calculator.Divide(4, 2);
+
+            //Assert
+            Assert.IsNotNull(integerResult);
+            Assert.AreEqual(2, integerResult); // Testing integers
+            Assert.AreEqual(0, calculator.Divide(5, 0));  //Divide by zero
+        }
+    }
+}
